9553ce7dd902bec8a0107350b129b31bbfdee707
Alessio Faraci <82022545+alefaraci@users.noreply.github.com>
2026-02-03 14:12:10 +0100

    Update init.lua

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -39,27 +39,26 @@ end
 local hyper = { 'cmd', 'alt', 'ctrl', 'shift' }
 
 -- Reload hammerspoon configs
-hs.hotkey.bind(hyper, "h", function() hs.reload() end)
+hs.hotkey.bind(hyper, "\\", function() hs.reload() end)
 -- Open Finder
 hs.hotkey.bind(hyper, "f", openOrActivateFinder)
 -- Switch Appearance
 hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, "d", AppearanceSwitcher)
 -- Applications
--- hs.hotkey.bind(hyper, "a", function() openApp("/System/Applications/Preview.app", "Preview") end)
--- hs.hotkey.bind(hyper, "b", function() openApp("/Applications/TeX/Bibdesk.app") end)
 hs.hotkey.bind(hyper, "z", function() openApp("/Applications/Zed.app", "Zed") end)
 hs.hotkey.bind(hyper, "c", function() openApp("/System/Applications/Calendar.app", "Calendar") end)
 hs.hotkey.bind(hyper, "e", function() openApp("/System/Applications/Mail.app", "Mail") end)
--- hs.hotkey.bind(hyper, "g", function() openApp("/Applications/GitHub Desktop.app", "GitHub") end)
+hs.hotkey.bind(hyper, "h", function() openApp("/Applications/GitHub Desktop.app", "GitHub") end)
 hs.hotkey.bind(hyper, "m", function() openApp("/System/Applications/Music.app", "Music") end)
+hs.hotkey.bind(hyper, "o", function() openApp("/Applications/Claude.app", "Claude") end)
+hs.hotkey.bind(hyper, "s", function() openApp("/Applications/Safari.app", "Safari") end)
+hs.hotkey.bind(hyper, "t", function() openApp("/Applications/Telegram.app", "Telegram") end)
+hs.hotkey.bind(hyper, "g", function() openApp("/Applications/Ghostty.app", "Ghostty") end)
+hs.hotkey.bind(hyper, "x", function() openApp("/Applications/Xcode.app", "Xcode") end)
+-- hs.hotkey.bind(hyper, "a", function() openApp("/System/Applications/Preview.app", "Preview") end)
+-- hs.hotkey.bind(hyper, "b", function() openApp("/Applications/TeX/Bibdesk.app") end)
 -- hs.hotkey.bind(hyper, "n", function() openApp("/System/Applications/Notes.app") end)
 -- hs.hotkey.bind(hyper, "n", function() openApp("/Applications/Numbers.app") end)
 -- hs.hotkey.bind(hyper, "k", function() openApp("/Applications/Skim.app") end)
--- hs.hotkey.bind(hyper, "o", function() openApp("/Applications/Claude.app", "Claude") end)
 -- hs.hotkey.bind(hyper, "i", function() openApp("/Users/alessiofaraci/Applications/Gemini.app") end)
-hs.hotkey.bind(hyper, "s", function() openApp("/Applications/Safari.app", "Safari") end)
-hs.hotkey.bind(hyper, "t", function() openApp("/Applications/Telegram.app", "Telegram") end)
--- hs.hotkey.bind(hyper, "v", function() openApp("/Applications/Visual Studio Code.app") end)
 -- hs.hotkey.bind(hyper, "x", function() openApp("/Applications/Texifier.app") end)
-hs.hotkey.bind(hyper, "g", function() openApp("/Applications/Ghostty.app", "Ghostty") end)
-hs.hotkey.bind(hyper, "x", function() openApp("/Applications/Xcode.app", "Xcode") end)
