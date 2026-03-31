883d929ca4de5d62314b8678434865df92ae5bfe
Alessio Faraci <82022545+alefaraci@users.noreply.github.com>
2026-03-31 16:57:35 +0200

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.cmd [[
   highlight NonText ctermbg=none
 ]]
 
+-- Sync clipboard between OS and Neovim.
 vim.g.clipboard = {
     name = 'OSC 52',
     copy = {
@@ -19,8 +20,7 @@ vim.g.clipboard = {
 }
 
 vim.opt.clipboard = "unnamedplus"
-
-vim.o.mouse = ''
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
 -- Convert tabs to spaces
 vim.opt.expandtab = true
@@ -33,3 +33,69 @@ vim.opt.shiftwidth = 4
 
 -- Set the soft tab width (for editing operations) to 4 spaces
 vim.opt.softtabstop = 4
+
+-- kikstart config
+
+-- Leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Make line numbers default
+vim.o.number = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Enable undo/redo changes even after closing and reopening a file
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+vim.o.confirm = true
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
+
+
